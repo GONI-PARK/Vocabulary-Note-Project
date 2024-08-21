0b26e93b1de50e4de173ea37baac3bdd28d2e3ba
박성곤 <nalsil139@gmail.com>
2024-08-21 20:17:36 +0900

    2024.08.21 20:17

---
+++ src/main/java/com/example/demo/VocabularyNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyNoteApplication.class, args);
+	}
+
+}
